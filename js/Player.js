9888f83812a11718db7d9d36cbfec566c04d90b9
ShivaliAich <74911636+ShivaliAich@users.noreply.github.com>
2021-12-21 14:03:45 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,37 @@
+class Player{
+    constructor (){
+        this.name = null;
+        this.character = "";
+        this.score = 0;
+        this.end = null;
+        this.index = null;
+        this.distanceX = 200;
+        this.distanceY = 0;
+        //this.badNPC = 0;
+    }
+    readPlayerCount(){
+      database.ref("playerCount").on('value',(data)=>{playerCount = data.val()});
+    }
+    updatePlayerCount(count){
+        console.log("hp1");
+        console.log(count);
+        database.ref("/").update({playerCount: count});
+
+    }
+    updatePlayerInfo(){
+        database.ref("players/player"+ this.index).set
+        ({name : this.name,
+        character : this.character,
+        score : this.score,
+        distanceY:this.distanceY,
+      distanceX:this.distanceX,
+        //badNPC: this.badNPC
+    });
+    }
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+}
